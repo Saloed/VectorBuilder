0362e0b3d74dad6363e5da42638d04c397f81b62
saloed <napster_1997@mail.ru>
2016-09-12 18:05:19 +0300

    unstable and stupy version

--- Embeddings/Construct.py
+++ Embeddings/Construct.py
@@ -27,20 +27,25 @@ def compute_rates(tokens):
                     child.left_rate = 1.0 - child.right_rate
 
 
-def random_change(tokens, layers, params, root_token):
+def random_change(tokens, layers, params, root_token, used_embeddings):
     def rand_token():
         return list(token_map.keys())[np.random.randint(0, len(token_map))]
 
-    swap_token_index = np.random.randint(root_token, len(tokens))
-    root_token = tokens[root_token]
+    swap_token_index = np.random.randint(0, len(tokens))
     current = tokens[swap_token_index]
     new_token = rand_token()
-    while current.token_type == new_token or root_token.token_type == new_token:
+    while current.token_type == new_token:
         new_token = rand_token()
     new_token_index = token_map[new_token]
     new_emb = params.embeddings[new_token_index]
-    layers[swap_token_index].bias = new_emb
-    return new_emb, new_token_index
+
+    if swap_token_index == root_token:
+        return new_emb
+    else:
+        if new_emb not in used_embeddings:
+            used_embeddings[new_token_index] = new_emb
+        layers[swap_token_index].bias = new_emb
+        return None
 
 
 def construct(tokens, params: Parameters, root_token_index, just_validation=False):
@@ -54,10 +59,11 @@ def construct(tokens, params: Parameters, root_token_index, just_validation=Fals
     assert root_token_index == 0
 
     root_token = tokens[root_token_index]
-    root_token_emb = params.embeddings[root_token.token_index]
+    positive_target = params.embeddings[root_token.token_index]
 
     root_layer = Layer(params.b_construct, "root_layer")
     layers[root_token_index] = root_layer
+    used_embeddings[root_token.token_index] = positive_target
 
     for i, node in enumerate(tokens):
         if i == root_token_index:
@@ -82,9 +88,10 @@ def construct(tokens, params: Parameters, root_token_index, just_validation=Fals
 
     neg_layers = deepcopy(layers)
 
-    one_more_param, token_index = random_change(tokens, neg_layers, params, root_token_index)
-    if not one_more_param in used_embeddings:
-        used_embeddings[token_index] = one_more_param
+    negative_target = random_change(tokens, neg_layers, params, root_token_index,
+                                    used_embeddings)
+
+    del used_embeddings[root_token.token_index]
 
     def f_builder(layer: Layer):
         if layer.forward is None:
@@ -97,32 +104,39 @@ def construct(tokens, params: Parameters, root_token_index, just_validation=Fals
     # def forward_propagation(network_layers: list):
     #     f_builder(network_layers[root_token_index])
     #     return function([], network_layers[root_token_index].forward)
-    @timing
+
+    # @timing
     def back_propagation(pos_forward, neg_forward):
         alpha = T.fscalar('alpha')
         decay = T.fscalar('decay')
-        target = root_token_emb
+        pos_target = positive_target
+        neg_target = negative_target
 
-        parameters = [params.b_construct]
-        parameters.extend(params.w.values())
-        parameters.extend(used_embeddings.values())
+        upd_params = [params.b_construct]
+        upd_params.extend(params.w.values())
+        upd_params.extend(used_embeddings.values())
 
-        pos_delta = target - pos_forward
-        neg_delta = target - neg_forward
+        pos_delta = pos_target - pos_forward
+
+        if neg_target is None:
+            neg_delta = neg_forward - pos_target
+        else:
+            neg_delta = neg_forward - neg_target
 
         pos_d = T.mul(T.sum(T.mul(pos_delta, pos_delta)), 0.5)
         neg_d = T.mul(T.sum(T.mul(neg_delta, neg_delta)), 0.5)
         error = T.nnet.relu(MARGIN + pos_d - neg_d)
 
-        target_update = neg_forward - pos_forward
-
         if not just_validation:
-            gparams = [T.grad(error, param) for param in parameters]
+            gparams = [T.grad(error, param) for param in upd_params]
             updates = [
                 (param, param - alpha * gparam - decay * alpha * param)
-                for param, gparam in zip(parameters, gparams)
+                for param, gparam in zip(upd_params, gparams)
                 ]
-            updates.append((target, target - target_update))
+
+            updates.append((pos_target, pos_target - pos_delta))
+            if neg_target is not None:
+                updates.append((neg_target, neg_target - neg_delta))
 
             return function([alpha, decay], outputs=error, updates=updates)
         else:
