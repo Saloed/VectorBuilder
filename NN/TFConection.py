7f7d96817179c29969f2aad07887ffc905afa2d9
saloed <napster_1997@mail.ru>
2017-02-28 17:38:09 +0300

    added tf parameters and NN primitives

---
+++ NN/TFConection.py
@@ -0,0 +1,39 @@
+from abc import abstractmethod
+import tensorflow as tf
+
+from NN.TFLayer import Layer
+
+
+class BaseConnection:
+    def __init__(self, from_layer: Layer, to_layer: Layer):
+        self.from_layer = from_layer
+        self.to_layer = to_layer
+        self.forward = None
+
+        from_layer.add_out_coonection(self)
+        to_layer.add_in_connection(self)
+
+    @abstractmethod
+    def build_forward(self):
+        pass
+
+    def __str__(self):
+        return 'con from {0} to {1}'.format(self.from_layer.name, self.to_layer.name)
+
+
+class Connection(BaseConnection):
+    def __init__(self, from_layer: Layer, to_layer: Layer, weights: tf.Variable, w_coeff=1.0):
+        super().__init__(from_layer, to_layer)
+        self.weights = weights
+        self.w_coeff = w_coeff
+
+    def build_forward(self):
+        self.forward = tf.multiply(tf.matmul(self.weights, self.from_layer.forward), self.w_coeff)
+
+
+class PoolConnection(BaseConnection):
+    def __init__(self, from_layer: Layer, to_layer: Layer):
+        super().__init__(from_layer, to_layer)
+
+    def build_forward(self):
+        self.forward = self.from_layer.forward
---
+++ TFAuthorClassifier/TFParameters.py
@@ -0,0 +1,57 @@
+import _pickle as P
+import tensorflow as tf
+from AuthorClassifier.ClassifierParams import *
+
+
+class Params:
+    def __init__(self,
+                 w_conv_root, w_conv_left, w_conv_right,
+                 w_hid, w_out,
+                 b_conv, b_hid, b_out,
+                 embeddings):
+        self.w = {
+            'w_conv_root': w_conv_root,
+            'w_conv_left': w_conv_left,
+            'w_conv_right': w_conv_right,
+            'w_hid': w_hid,
+            'w_out': w_out,
+        }
+        self.b = {
+            'b_conv': b_conv,
+            'b_hid': b_hid,
+            'b_out': b_out
+        }
+
+        self.embeddings = embeddings
+
+
+def rand_weight(shape_0, shape_1, name):
+    return tf.Variable(
+        tf.truncated_normal(shape=[shape_0, shape_1], stddev=RANDOM_RANGE),
+        name=name)
+
+
+def rand_bias(shape, name):
+    return tf.Variable(
+        tf.truncated_normal(shape=[shape], stddev=RANDOM_RANGE),
+        name=name)
+
+
+def init_params(author_amount):
+    with open('TFAuthorClassifier/embeddings', 'rb') as f:
+        np_embs = P.load(f)
+    embeddings = {name: tf.constant(val, dtype=tf.float32, name=name) for name, val in np_embs.items()}
+
+    w_conv_root = rand_weight(NUM_CONVOLUTION, NUM_FEATURES, 'w_conv_root')
+    w_conv_left = rand_weight(NUM_CONVOLUTION, NUM_FEATURES, 'w_conv_left')
+    w_conv_right = rand_weight(NUM_CONVOLUTION, NUM_FEATURES, 'w_conv_right')
+    w_hid = rand_weight(NUM_HIDDEN, NUM_CONVOLUTION, 'w_hid')
+    w_out = rand_weight(author_amount, NUM_HIDDEN, 'w_out')
+
+    b_conv = rand_bias(NUM_CONVOLUTION, 'b_conv')
+    b_hid = rand_bias(NUM_HIDDEN, 'b_hid')
+    b_out = rand_bias(author_amount, 'b_out')
+
+    return Params(w_conv_root, w_conv_left, w_conv_right,
+                  w_hid, w_out, b_conv, b_hid, b_out,
+                  embeddings)
